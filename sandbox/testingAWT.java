63ec70df0828db04b2799d7c68cf5842f7d353a7
Phillip D'Andrea <phd016@gmail.com>
2023-08-04 23:08:51 -0400

    start tutorial

---
+++ sandbox/testingAWT.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class Game extends JFrame{
+
+    public Game() {
+
+        setSize(400,400);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Start of tutorial");
+
+        Game game = new Game()
+    }
+}
